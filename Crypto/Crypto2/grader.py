285cad3bea660e1a19449861b4cc749ff45096d3
Elijah3001 <elwilliams@tracylc.net>
2017-03-27 17:12:34 -0700

    Update grader.py

--- Crypto/Crypto2/grader.py
+++ Crypto/Crypto2/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-  if 'Cyber Defender' in key:
+  if 'Cyber Defender'or'CYBER DEFENDER'or'cyber defender'or 'CYBERDEFENDER' or 'cyberdefender' in key:
     return True, "Good work!"
   else:
     return False, "Nope"
