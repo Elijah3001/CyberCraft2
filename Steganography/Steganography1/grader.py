369acca87bfba85cb0d8af42e9dbe2e16f7b33d2
Elijah3001 <elwilliams@tracylc.net>
2017-05-02 11:52:06 -0700

    Update grader.py

--- Steganography/Steganography1/grader.py
+++ Steganography/Steganography1/grader.py
@@ -1,5 +1,6 @@
+#In order to solve this problems you will need to do a few steps. First make sure PIL is installed on the device, Second save the image, Third download the code, Fourth, make sure the picture and code is on the desktop, Fifth go into the terminal and make sure you're on the desktop, Sixth type in "python hide.py -d picture.png" this will decode the hidden message within the image and return "keep it going" as your final answer. 
 def grade(arg, key):
-  if 'keep it going' in key:
+  if 'keep it going' or 'keepitgoing' or 'KEEPITGOING' or 'KEEP IT GOING' in key:
     return True, "Good work!"
   else:
     return False, "Nope"
