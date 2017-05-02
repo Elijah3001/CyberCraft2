e7b70d80c074df28dfbacd21dbecce6f1807a411
Elijah3001 <elwilliams@tracylc.net>
2017-05-02 11:50:14 -0700

    Update grader.py

--- HTML/HTML1/grader.py
+++ HTML/HTML1/grader.py
@@ -1,7 +1,6 @@
+#In order to solve this problem you must take the binary you find on the HTML page and convert it into text. The final answer is "keep it going"
 def grade(arg, key):
-  if 'keep it going' in key:
+  if 'keep it going' or 'keepitgoing' or 'KEEPITGOING' or 'KEEP IT GOING'in key:
     return True, "Good work!"
   else:
     return False, "Nope"
-
-
