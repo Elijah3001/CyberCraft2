4e51cbf27a0ab8eaa6eea56973fc872d1c13afbc
Elijah3001 <elwilliams@tracylc.net>
2017-05-02 11:57:42 -0700

    Update grader.py

--- Trivia/Trivia1/grader.py
+++ Trivia/Trivia1/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-  if 'Linus Torvalds' in key:
+  if 'Linus Torvalds'or'LinusTorvalds'or'LINUSTOVALDS'or'LINUS TOVALDS' in key:
     return True, "Good work!"
   else:
     return False, "Nope"
