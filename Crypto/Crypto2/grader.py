b20b3ec98ad13a84a7ac0c1222a6c2ef4a11ac73
Elijah3001 <elwilliams@tracylc.net>
2017-05-02 11:49:00 -0700

    Update grader.py

--- Crypto/Crypto2/grader.py
+++ Crypto/Crypto2/grader.py
@@ -1,3 +1,4 @@
+#In order to solve this problem you must take "Jfily Klmlukly" and put it through a caesar cipher you will then get in return "Cyber Defender" as your final answer.
 def grade(arg, key):
   if 'Cyber Defender'or'CYBER DEFENDER'or'cyber defender'or 'CYBERDEFENDER' or 'cyberdefender' in key:
     return True, "Good work!"
