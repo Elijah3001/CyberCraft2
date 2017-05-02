490a1c766a554e468cc7b8b13837653a802a0795
Elijah3001 <elwilliams@tracylc.net>
2017-05-02 11:51:13 -0700

    Update grader.py

--- Sequence/Sequence2/grader.py
+++ Sequence/Sequence2/grader.py
@@ -1,3 +1,4 @@
+#In order to solve this problem you must first take the original number which is "54" and increase the number you multiply it by each time. So take "54" times it by 3 you will get "162" times "162" by 4 and you get "648" and so on. 
 def grade(arg, key):
   if '136080' in key:
     return True, "Good work!"
